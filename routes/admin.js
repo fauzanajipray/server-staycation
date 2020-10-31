107c8eeb7860edfe7864fbaef04c02ad6f418dbe
Fauzan Aji Prayoga <fauzanjr1@gmail.com>
2020-10-31 12:26:00 +0700

    add view/admin ..

--- routes/admin.js
+++ routes/admin.js
@@ -3,9 +3,8 @@ var router = express.Router();
 const adminController = require("../controller/adminController");
 
 router.get("/dashboard", adminController.viewDasboard);
-
-// router.get("/dashboard", (res, req) => {
-//   res.render("/admin/dashboard/view_dashboard");
-// });
-
+router.get("/category", adminController.viewCategory);
+router.get("/bank", adminController.viewBank);
+router.get("/item", adminController.viewItem);
+router.get("/booking", adminController.viewBooking);
 module.exports = router;
